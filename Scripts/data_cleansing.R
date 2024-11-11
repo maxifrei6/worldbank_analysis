22cc12f8fd7836c329ac11f0f3932dd55b5b6534
maxifrei6 <maximilian.frei@campus.lmu.de>
2024-11-11 19:50:13 +0100

    adding co2data to full_data

--- Scripts/data_cleansing.R
+++ Scripts/data_cleansing.R
@@ -27,8 +27,17 @@ library(stringr)
 library(checkmate)
 source("Scripts/functions.R")
 
-# Read the csv file from Data/Raw folder
+# Read the csv files from Data/Raw folder
 geodata <- read.csv("Data/Raw/Worldbank2.csv", header = TRUE, sep = ",", na.strings = "")
+co2data <- read.csv("Data/Raw/Co2_emi_WB.csv", header = TRUE, sep = ";", na.strings = "")
+
+# Remove only the first instance of a dot if it is followed by another dot in the number (co2 emissions format x.xxx.xxx)
+co2data <- co2data %>%
+  mutate(across(everything(), ~ str_replace(., "(?<=\\d)\\.(?=\\d+\\.\\d*)", "")))
+         
+str(geodata)
+str(co2data)
+head(co2data)
 
 # Read the xlsx file from Data/Row folder
 sociometrics <- read_excel("Data/Raw/Worldbank1.xlsx", sheet = 1, col_names = TRUE, na = "..")
@@ -37,51 +46,81 @@ sociometrics <- read_excel("Data/Raw/Worldbank1.xlsx", sheet = 1, col_names = TR
 # Apply the remove_NAs function to all interested datasets
 geodata <- remove_NA(geodata)
 sociometrics <- remove_NA(sociometrics)
+co2data <- remove_NA(co2data)
+
+# Remove duplicate rows if any
+geodata <- distinct(geodata)
+sociometrics <- distinct(sociometrics)
+co2data <- distinct(co2data)
 
 # Get an overview of the datasets
 str(geodata)
 summary(geodata)
 str(sociometrics)
 summary(sociometrics)
+str(co2data)
+summary(co2data)
 
 # Modification to datasets, simplifying their joining
 # Colnames for years aligning in both datasets
 colnames(geodata) <- gsub(pattern = "X([[:digit:]]{4})..YR[[:digit:]]{4}.",
                           replacement = "\\1", colnames(geodata))
+colnames(co2data) <- gsub(pattern = "X([[:digit:]]{4})..YR[[:digit:]]{4}.",
+                          replacement = "\\1", colnames(co2data))
 colnames(sociometrics) <- gsub(pattern = "([[:digit:]]{4}) \\[YR\\1\\]",
                                replacement = "\\1", colnames(sociometrics))
-# Remaining colnames in geodata aligning with sociometrics, i.e. replacing dot between words with space
+# Remaining colnames in geodata and co2data aligning with sociometrics, i.e. replacing dot between words with space
 geodata <- geodata %>%
   mutate(across(where(is.character))) %>%
   rename_with(~ gsub(pattern = "\\.", replacement = " ", .))
+co2data <- co2data %>%
+  mutate(across(where(is.character))) %>%
+  rename_with(~ gsub(pattern = "\\.", replacement = " ", .))
+
 
 # Removing metadata rows
 metadata_source <- "Data from database: World Development Indicators" 
 metadata_time <- "Last Updated: 10/24/2024"
 geodata <- geodata[geodata$`Country Name` != metadata_source &
                      geodata$`Country Name` != metadata_time, ]
+co2data <- co2data[co2data$`Country Name` != metadata_source &
+                     co2data$`Country Name` != metadata_time, ]
 sociometrics <- sociometrics[sociometrics$`Country Name` != metadata_source &
                                sociometrics$`Country Name` != metadata_time, ]
 
 
 
 # TODO (???)
+str(geodata)
+str(sociometrics)
+str(co2data)
 
+# Delete column "average" and "Series Name" to avoid data type issues
+geodata <- geodata %>%
+  select(c(-average, -`Series Name`))
+sociometrics <- sociometrics %>%
+  select(c(-average, -`Series Name`))
+co2data <- co2data %>%
+  select(c(-average, -`Series Name`))
 
+# Ensure all year columns  are numeric to avoid merge errors
+geodata <- geodata %>%
+  mutate(across(matches("^[0-9]{4}$"), as.numeric))
+sociometrics <- sociometrics %>%
+  mutate(across(matches("^[0-9]{4}$"), as.numeric))
+co2data <- co2data %>%
+  mutate(across(matches("^[0-9]{4}$"), as.numeric))
 
 # Merge both datasets into one full dataset containing all information
-full_data <- full_join(geodata, sociometrics, by = colnames(geodata))
+full_data <- full_join(geodata, sociometrics, by = colnames(geodata)) %>%
+  full_join(co2data, by = colnames(geodata))
+
 
 # Sort the joined dataset by Country Name
 full_data <- sort_by(full_data, full_data$`Country Name`)
 
 
 # TODO
-
-
-# Delete column "average" and "Series Name"
-full_data <- full_data %>%
-  select(c(-average, -`Series Name`))
 
 # Columns with Years in column "Year"
 full_data <- full_data %>%
@@ -92,7 +131,7 @@ full_data <- full_data %>%
     values_drop_na = FALSE
   )
 
-# Variables in "Series Name" as columns
+# Variables in "Series Code" as columns
 full_data <- full_data %>%
   pivot_wider(
     id_cols = c(`Country Name`, `Country Code`, Year),
