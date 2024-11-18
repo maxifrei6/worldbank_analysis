b643bff22071c48f33e0f32042f0ff49f13c1cb7
MertesLe <151747969+MertesLe@users.noreply.github.com>
2024-11-18 09:36:52 +0100

    Data Info Leonie added

--- Scripts/hiv.R
+++ Scripts/hiv.R
@@ -1,1 +1,4 @@
 # HIV
+
+# HIV Variable: Prevalence of HIV, total (% of population ages 15-49)
+#               SH.DYN.AIDS.ZS
--- Scripts/tobacco_LEONIE.R
+++ Scripts/tobacco_LEONIE.R
@@ -1,1 +1,15 @@
 # Tobacco_LEONIE
+
+# How does GDP per capita relate to the prevalence of current tobacco use 
+# (% of adults)?
+
+# Variables:  GDP per capita, PPP (constant 2021 international $)
+#             NY.GDP.PCAP.PP.KD
+
+#             Prevalence of current tobacco use (% of adults)
+#             SH.PRV.SMOK
+
+# dotplot:  x-Achse:  GDP per capita (BIP pro Kopf)
+#           y-Achse:  Prevalence of current tobacco use (% of adults)
+#           colour:   Country
+#                     -> grouped? -> mean?
