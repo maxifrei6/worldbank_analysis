3394f8d2dcb50a0a13a6ece39132b969327375fd
MertesLe <151747969+MertesLe@users.noreply.github.com>
2024-11-24 23:27:09 +0100

    mainly work on grouped data, HIV, Tobacco

--- Scripts/hiv.R
+++ Scripts/hiv.R
@@ -6,11 +6,11 @@ library(ggplot2)
 ## HIV Variable:  Prevalence of HIV, total (% of population ages 15-49)
 #                 SH.DYN.AIDS.ZS
 
-#                 Total alcohol consumption per capita 
+#                 Total alcohol consumption per capita
 #                 (liters of pure alcohol, projected estimates, 15+ years of age)
 #                 SH.ALC.PCAP.LI
 
-#                 Labor force with basic education 
+#                 Labor force with basic education
 #                 (% of total working-age population with basic education)
 #                 SL.TLF.BASC.ZS
 
@@ -23,13 +23,13 @@ library(ggplot2)
 ## Answer / Visualization of 1)
 # 1.1) last available data with alcohol counsumption on x-axis and HIV prevalence on y-axis
 #       countries/continents coloured
-# 1.2)  
-# 1.3)  
-# 1.4)  
+# 1.2)
+# 1.3)
+# 1.4)
 # 1.5) facetting by country, alcoholconsumption vs HIV
 # 1.6) facetting by country, time vs HIV
-# 1.7)  
-# 1.8)  
+# 1.7)
+# 1.8)
 
 ## Answer / Visualization of 2)
 # 2.1) grouping of countries by level of education (2 to 4 groups)
@@ -38,10 +38,10 @@ library(ggplot2)
 #       dotplot of last available data
 #       countrie coded on x-scale?
 # 2.3) boxplot with grouped countries by education and HIV prevalence values
-# 2.4) facetting by Year, countries over colour, corr of education and HIV displayed 
+# 2.4) facetting by Year, countries over colour, corr of education and HIV displayed
 # 2.5) facetting by country, education vs HIV
-# 2.6) y-axis: HIV Prevalence per Labour Force with basic information (HIV/EDU) 
-# 2.a) grouping countries by development level of Worldbank  
+# 2.6) y-axis: HIV Prevalence per Labour Force with basic information (HIV/EDU)
+# 2.a) grouping countries by development level of Worldbank
 # 2.b) grouping countries by continent
 
 ## Adjust dataframe
@@ -61,16 +61,16 @@ str(hiv_df$SL.TLF.BASC.ZS)
 # NA`s in Prevalence of HIV
 sum(is.na(hiv_df$SH.DYN.AIDS.ZS))
 # NA's per year
-hiv_years_data <- hiv_df %>% 
-  group_by(Year) %>% 
-  reframe(num_na_hiv = sum(is.na(SH.DYN.AIDS.ZS))) %>% 
+hiv_years_data <- hiv_df %>%
+  group_by(Year) %>%
+  reframe(num_na_hiv = sum(is.na(SH.DYN.AIDS.ZS))) %>%
   filter(num_na_hiv < 25)
 # NA`s in alcohol consumption
 sum(is.na(hiv_df$SH.ALC.PCAP.LI))
 # NA's per year
-alc_years_data <- hiv_df %>% 
-  group_by(Year) %>% 
-  reframe(num_na_alc = sum(is.na(SH.ALC.PCAP.LI))) %>% 
+alc_years_data <- hiv_df %>%
+  group_by(Year) %>%
+  reframe(num_na_alc = sum(is.na(SH.ALC.PCAP.LI))) %>%
   filter(num_na_alc < 25)
 # NA`s in Labor force with basic education
 sum(is.na(hiv_df$SL.TLF.BASC.ZS))
@@ -93,8 +93,8 @@ which(is.na(hiv_df$SH.ALC.PCAP.LI))
 which(is.na(hiv_df$SH.DYN.AIDS.ZS))
 # possibly no data about following countries basic education:
 j <- 1
-for (i in seq_len(25)){
-  if (j %in% which(is.na(hiv_df$SL.TLF.BASC.ZS))){
+for (i in seq_len(25)) {
+  if (j %in% which(is.na(hiv_df$SL.TLF.BASC.ZS))) {
     print(hiv_df[j, "Country Name"])
   }
   j <- j + 22
@@ -109,71 +109,135 @@ for (i in seq_len(25)){
 # no data about United States hiv rate
 
 ## data in year:
-hiv_year_df <- function(year){
-  hiv_df %>% 
+hiv_year_df <- function(year) {
+  hiv_df %>%
     filter(Year %in% year)
 }
 
 ## 1.1)
 # Dataframe adjusted (no data for Aruba)
-hiv_alc_df <- hiv_df %>% 
-  filter(`Country Name` != "Aruba") %>% 
+hiv_alc_df <- hiv_df %>%
+  filter(`Country Name` != "Aruba") %>%
   select(-SL.TLF.BASC.ZS)
-hiv_alc_2020_df <- hiv_alc_df %>% 
+hiv_alc_2020_df <- hiv_alc_df %>%
   filter(Year == 2020)
 
 # plot results over countries
-ggplot(hiv_alc_2020_df, aes(SH.ALC.PCAP.LI, SH.DYN.AIDS.ZS, colour = `Country Name`)) +
+ggplot(hiv_alc_2020_df,
+       aes(SH.ALC.PCAP.LI, SH.DYN.AIDS.ZS, colour = `Country Name`)) +
   geom_point() +
-  labs(x = "Alkoholkonsum in Litern pro Kopf", y = "HIV Prävalenz in %",
-       title = "Alkoholkonsum vs. HIV Prävalenz", colour = "Land")
+  labs(
+    x = "Alkoholkonsum in Litern pro Kopf",
+    y = "HIV Prävalenz in %",
+    title = "Alkoholkonsum vs. HIV Prävalenz",
+    colour = "Land"
+  )
 # plot results over continents
-hiv_alc_2020_continent_df <- hiv_alc_2020_df %>% 
-  group_by(continent) %>% 
-  summarise(SH.ALC.PCAP.LI = mean(SH.ALC.PCAP.LI, na.rm = TRUE),
-            SH.DYN.AIDS.ZS = mean(SH.DYN.AIDS.ZS, na.rm = TRUE))
-ggplot(hiv_alc_2020_continent_df, aes(SH.ALC.PCAP.LI, SH.DYN.AIDS.ZS, colour = continent)) +
+hiv_alc_2020_continent_df <- hiv_alc_2020_df %>%
+  group_by(continent) %>%
+  summarise(
+    SH.ALC.PCAP.LI = mean(SH.ALC.PCAP.LI, na.rm = TRUE),
+    SH.DYN.AIDS.ZS = mean(SH.DYN.AIDS.ZS, na.rm = TRUE)
+  )
+ggplot(hiv_alc_2020_continent_df,
+       aes(SH.ALC.PCAP.LI, SH.DYN.AIDS.ZS, colour = continent)) +
   geom_point() +
-  labs(x = "Alkoholkonsum in Litern pro Kopf", y = "HIV Prävalenz in %",
-       title = "Alkoholkonsum vs. HIV Prävalenz", colour = "Kontinent")
+  labs(
+    x = "Alkoholkonsum in Litern pro Kopf",
+    y = "HIV Prävalenz in %",
+    title = "Alkoholkonsum vs. HIV Prävalenz",
+    colour = "Kontinent"
+  )
 # as comparison: here countries coded over their continent, to take a look at variance, sample size, ...
-ggplot(hiv_alc_2020_df, aes(SH.ALC.PCAP.LI, SH.DYN.AIDS.ZS, colour = continent)) +
+ggplot(hiv_alc_2020_df,
+       aes(SH.ALC.PCAP.LI, SH.DYN.AIDS.ZS, colour = continent)) +
   geom_point(size = 3, alpha = 0.5) +
-  labs(x = "Alkoholkonsum in Litern pro Kopf", y = "HIV Prävalenz in %",
-       title = "Alkoholkonsum vs. HIV Prävalenz", colour = "Kontinent")
+  labs(
+    x = "Alkoholkonsum in Litern pro Kopf",
+    y = "HIV Prävalenz in %",
+    title = "Alkoholkonsum vs. HIV Prävalenz",
+    colour = "Kontinent"
+  )
 
-# 1.5)
-ggplot(hiv_alc_df,
-       aes(SH.ALC.PCAP.LI, SH.DYN.AIDS.ZS, color = Year)) +
+# plot results over development status
+hiv_alc_2020_dev_df <- hiv_alc_2020_df %>%
+  group_by(dev_status) %>%
+  summarise(
+    SH.DYN.AIDS.ZS = mean(SH.DYN.AIDS.ZS, na.rm = TRUE),
+    SH.ALC.PCAP.LI = mean(SH.ALC.PCAP.LI, na.rm = TRUE)
+  )
+ggplot(hiv_alc_2020_dev_df,
+       aes(SH.ALC.PCAP.LI, SH.DYN.AIDS.ZS, colour = dev_status)) +
+  geom_point() +
+  labs(
+    x = "Alkoholkonsum pro Kopf in Litern",
+    y = "HIV Prävalenz",
+    colour = "Entwicklungstand",
+    title = "Alkoholkonsum vs HIV Prävalenz im Jahr 2020"
+  )
+# for comparison all countries but only coded by development state (variance, etc ...)
+ggplot(hiv_alc_2020_df,
+       aes(SH.ALC.PCAP.LI, SH.DYN.AIDS.ZS, colour = dev_status)) +
+  geom_point() +
+  labs(
+    x = "Alkoholkonsum pro Kopf in Litern",
+    y = "HIV Prävalenz",
+    colour = "Entwicklungsstand des Landes",
+    title = "Alkoholkonsum vs HIV Prävalenz im Jahr 2020"
+  )
+
+## 1.5)
+ggplot(hiv_alc_df, aes(SH.ALC.PCAP.LI, SH.DYN.AIDS.ZS, color = Year)) +
   geom_point(size = 1) +
-  facet_wrap(~ `Country Name`) +
-  geom_smooth(method = "lm", se = FALSE, color = "black", size = 0.5) +
+  facet_wrap( ~ `Country Name`) +
+  geom_smooth(
+    method = "lm",
+    se = FALSE,
+    color = "black",
+    size = 0.5
+  ) +
   labs(y = "HIV Prävalenz in %", x = "totaler Alkoholkonsum pro Kopf", color = "Jahr")
 
 # 1.6)
-ggplot(hiv_alc_df,
-       aes(Year, SH.DYN.AIDS.ZS, color = SH.ALC.PCAP.LI)) +
+ggplot(hiv_alc_df, aes(Year, SH.DYN.AIDS.ZS, color = SH.ALC.PCAP.LI)) +
   geom_point(size = 1) +
-  facet_wrap(~ `Country Name`) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 4)) +
-  labs(x = "Jahr", y = "HIV Prävalenz in %", color = "totaler Alkoholkonsum pro Kopf",
-       title = "HIV Prälenz über Zeit sowie Alkoholkonsum")
+  facet_wrap( ~ `Country Name`) +
+  theme(axis.text.x = element_text(
+    angle = 45,
+    hjust = 1,
+    size = 4
+  )) +
+  labs(
+    x = "Jahr",
+    y = "HIV Prävalenz in %",
+    color = "totaler Alkoholkonsum pro Kopf",
+    title = "HIV Prälenz über Zeit sowie Alkoholkonsum"
+  )
 
-## 2.4) 
+## 2.4)
 ggplot(hiv_df,
        aes(SL.TLF.BASC.ZS, SH.DYN.AIDS.ZS, color = `Country Name`)) +
   geom_point(size = 1) +
   facet_wrap(~ Year) +
-  geom_smooth(method = "lm", se = FALSE, color = "black", size = 0.5) +
+  geom_smooth(
+    method = "lm",
+    se = FALSE,
+    color = "black",
+    size = 0.5
+  ) +
   labs(x = "HIV Prävalenz", y = "Bildungsstand der Erwerbstätigen", color = "Land")
 # maybe add rate of change of linear regression line
 
 ## 2.5)
-ggplot(hiv_df,
-       aes(SL.TLF.BASC.ZS, SH.DYN.AIDS.ZS, color = Year)) +
+ggplot(hiv_df, aes(SL.TLF.BASC.ZS, SH.DYN.AIDS.ZS, color = Year)) +
   geom_point(size = 1) +
-  facet_wrap(~ `Country Name`) +
-  geom_smooth(method = "lm", se = FALSE, color = "black", size = 0.5) +
+  facet_wrap( ~ `Country Name`) +
+  geom_smooth(
+    method = "lm",
+    se = FALSE,
+    color = "black",
+    size = 0.5
+  ) +
   labs(y = "HIV Prävalenz in %", x = "% Erwerbstätige mit Grundbildung", color = "Jahr")
 
 ## 2.6)
@@ -181,14 +245,17 @@ ggplot(hiv_df,
 #   low value: low HIV or high HIV but high edu, high edu or high HIV and high Edu
 summary(hiv_df$SH.DYN.AIDS.ZS)
 summary(hiv_df$SL.TLF.BASC.ZS)
-ggplot(hiv_df,
-       aes(Year, SH.DYN.AIDS.ZS/SL.TLF.BASC.ZS)) +
+ggplot(hiv_df, aes(Year, SH.DYN.AIDS.ZS / SL.TLF.BASC.ZS)) +
   geom_point(size = 1) +
-  facet_wrap(~ `Country Name`) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 5)) +
+  facet_wrap( ~ `Country Name`) +
+  theme(axis.text.x = element_text(
+    angle = 45,
+    hjust = 1,
+    size = 5
+  )) +
   labs(x = "Jahr", y = "HIV Prävalenz / Erwerbstätige mit Bildung")
 
-## 2.a) 
+## 2.a)
 # Categorize all 25 countries: (AI, https://deepai.org/chat/free-chatgpt)
 # (Entwicklungs-, Schwellen- und hoch entwickelte Länder)
 development_ct <- c(
@@ -238,46 +305,96 @@ hiv_df$dev_status <- ifelse(
     )
   )
 )
-# Because the grouping by development status is of relative fresh data, we only 
+# Because the grouping by development status is of relative fresh data, we only
 # take a look at year 2021; we can delete alc column (doesn't have any values)
-hiv_development_df <- hiv_df %>% 
-  filter(Year == 2021) %>% 
-  select(- SH.ALC.PCAP.LI)
+hiv_development_df <- hiv_df %>%
+  filter(Year == 2021) %>%
+  select(-SH.ALC.PCAP.LI)
 # Dataframe of grouped countries and their value
 hiv_development_df <- hiv_development_df %>%
   group_by(dev_status) %>%
-  summarise(SH.DYN.AIDS.ZS = mean(SH.DYN.AIDS.ZS, na.rm = TRUE),
-            SL.TLF.BASC.ZS = mean(SL.TLF.BASC.ZS, na.rm = TRUE))
+  summarise(
+    SH.DYN.AIDS.ZS = mean(SH.DYN.AIDS.ZS, na.rm = TRUE),
+    SL.TLF.BASC.ZS = mean(SL.TLF.BASC.ZS, na.rm = TRUE)
+  )
 # keep year-information (funktioniert nicht)
 hiv_df %>%
   select(-SH.ALC.PCAP.LI) %>%
-  group_by(Year) %>% 
+  group_by(Year) %>%
   group_by(dev_status) %>%
-  summarise(SH.DYN.AIDS.ZS = mean(SH.DYN.AIDS.ZS, na.rm = TRUE),
-            SL.TLF.BASC.ZS = mean(SL.TLF.BASC.ZS, na.rm = TRUE))  
+  summarise(
+    SH.DYN.AIDS.ZS = mean(SH.DYN.AIDS.ZS, na.rm = TRUE),
+    SL.TLF.BASC.ZS = mean(SL.TLF.BASC.ZS, na.rm = TRUE)
+  )
 
 ## Visualising results of results
-ggplot(hiv_development_df, aes(SL.TLF.BASC.ZS, SH.DYN.AIDS.ZS, colour = dev_status)) + 
+ggplot(hiv_development_df,
+       aes(SL.TLF.BASC.ZS, SH.DYN.AIDS.ZS, colour = dev_status)) +
   geom_point() +
-  labs(title = "Korrelation zwischen HIV Auftreten und Bildungsstand", 
-       y = "HIV Prävalenz", x = "Bildungsstand der Erwerbstätigen",
-       colour = "Entwicklungsstand")
+  labs(
+    title = "Korrelation zwischen HIV Auftreten und Bildungsstand",
+    y = "HIV Prävalenz",
+    x = "Bildungsstand der Erwerbstätigen",
+    colour = "Entwicklungsstand"
+  )
 # stimmen die Werte wirklich?
 
 ## 2b)
 continent_mapping <- data.frame(
-  Country = c('Afghanistan', 'Aruba', 'Bangladesh', 'Bolivia', 'Brazil', 
-           'Cambodia', 'Chad', 'Russian Federation', 'Tanzania', 'Thailand', 
-           'United States', 'United Kingdom', 'Nigeria', 'Viet Nam', 
-           'China', 'Czechia', 'Finland', 'Ghana', 'India', 
-           'Kazakhstan', 'Mali', 'New Zealand', 'Pakistan', 'Peru', 
-           'Qatar'),
-  continent = c('Asien', 'Nordamerika', 'Asien', 'Südamerika', 'Südamerika', 
-                'Asien', 'Afrika', 'Europa', 'Afrika', 'Asien', 
-                'Nordamerika', 'Europa', 'Afrika', 'Asien', 
-                'Asien', 'Europa', 'Europa', 'Afrika', 'Asien', 
-                'Asien', 'Afrika', 'Ozeanien', 'Asien', 'Südamerika', 
-                'Asien')
+  Country = c(
+    'Afghanistan',
+    'Aruba',
+    'Bangladesh',
+    'Bolivia',
+    'Brazil',
+    'Cambodia',
+    'Chad',
+    'Russian Federation',
+    'Tanzania',
+    'Thailand',
+    'United States',
+    'United Kingdom',
+    'Nigeria',
+    'Viet Nam',
+    'China',
+    'Czechia',
+    'Finland',
+    'Ghana',
+    'India',
+    'Kazakhstan',
+    'Mali',
+    'New Zealand',
+    'Pakistan',
+    'Peru',
+    'Qatar'
+  ),
+  continent = c(
+    'Asien',
+    'Nordamerika',
+    'Asien',
+    'Südamerika',
+    'Südamerika',
+    'Asien',
+    'Afrika',
+    'Europa',
+    'Afrika',
+    'Asien',
+    'Nordamerika',
+    'Europa',
+    'Afrika',
+    'Asien',
+    'Asien',
+    'Europa',
+    'Europa',
+    'Afrika',
+    'Asien',
+    'Asien',
+    'Afrika',
+    'Ozeanien',
+    'Asien',
+    'Südamerika',
+    'Asien'
+  )
 )
 # dataframe with continent variable
 hiv_df <- hiv_df %>%
@@ -285,17 +402,26 @@ hiv_df <- hiv_df %>%
 # grouped dataframe with values by mean over all years
 hiv_continent_df <- hiv_df %>%
   group_by(continent) %>%
-  summarise(SH.DYN.AIDS.ZS = mean(SH.DYN.AIDS.ZS, na.rm = TRUE),
-            SL.TLF.BASC.ZS = mean(SL.TLF.BASC.ZS, na.rm = TRUE))
+  summarise(
+    SH.DYN.AIDS.ZS = mean(SH.DYN.AIDS.ZS, na.rm = TRUE),
+    SL.TLF.BASC.ZS = mean(SL.TLF.BASC.ZS, na.rm = TRUE)
+  )
 # only year 2021
-hiv_continent_2021_df <- hiv_df %>% 
-  filter(Year == 2021) %>% 
-  select(-SH.ALC.PCAP.LI) %>% 
-  group_by(continent) %>% 
-  summarise(SH.DYN.AIDS.ZS = mean(SH.DYN.AIDS.ZS, na.rm = TRUE),
-            SL.TLF.BASC.ZS = mean(SL.TLF.BASC.ZS, na.rm = TRUE))
+hiv_continent_2021_df <- hiv_df %>%
+  filter(Year == 2021) %>%
+  select(-SH.ALC.PCAP.LI) %>%
+  group_by(continent) %>%
+  summarise(
+    SH.DYN.AIDS.ZS = mean(SH.DYN.AIDS.ZS, na.rm = TRUE),
+    SL.TLF.BASC.ZS = mean(SL.TLF.BASC.ZS, na.rm = TRUE)
+  )
 # plot results
-ggplot(hiv_continent_2021_df, aes(SL.TLF.BASC.ZS, SH.DYN.AIDS.ZS, colour = continent)) +
+ggplot(hiv_continent_2021_df,
+       aes(SL.TLF.BASC.ZS, SH.DYN.AIDS.ZS, colour = continent)) +
   geom_point() +
-  labs(x = "% Erwerbstätige mit Grundbildung", y = "HIV Prävalenz in %",
-       title = "Grundbildung vs. HIV Prävalenz", colour = "Kontinent")
+  labs(
+    x = "% Erwerbstätige mit Grundbildung",
+    y = "HIV Prävalenz in %",
+    title = "Grundbildung vs. HIV Prävalenz",
+    colour = "Kontinent"
+  )
--- Scripts/tobacco_LEONIE.R
+++ Scripts/tobacco_LEONIE.R
@@ -2,16 +2,20 @@
 
 library(dplyr)
 library(ggplot2)
-## How does GDP per capita relate to the prevalence of current tobacco use 
+## How does GDP per capita relate to the prevalence of current tobacco use
 # (% of adults)?
-#   Options of completing this task:    
+#   Options of completing this task:
 #     1)  in each Country: every year a dot (problem: NA's)
 #     2)  whole data: mean of each country one dot
 #     3)  last data used for evaluation -> Year 2021
-#     4)  facetting by year
+#     4)  Facetting by year
 #     5)  Bar chart comparing GDP to tabacco usage
 #     6)  Facetting by country, x-axis = Year, y-axis1 = GDP, y-axis2 = tobacco usage
 #     7)  Facetting by country, x-axis = Year, y-axis = tobacco usage
+#     8)  Data grouped by development state of 2020
+#     9)  Data grouped by continent
+# a) grouping data by development state
+# b) grouping data by continent
 
 # Variables:  GDP per capita, PPP (constant 2021 international $)
 #             NY.GDP.PCAP.PP.KD
@@ -34,6 +38,143 @@ tobacco_df <- full_data %>%
 View(tobacco_df)
 str(tobacco_df$NY.GDP.PCAP.PP.KD)
 str(tobacco_df$SH.PRV.SMOK)
+# a)
+# Categorize all 25 countries: (AI, https://deepai.org/chat/free-chatgpt)
+# (Entwicklungs-, Schwellen- und hoch entwickelte Länder)
+development_ct <- c(
+  "Afghanistan",
+  "Bangladesh",
+  "Bolivia",
+  "Cambodia",
+  "Chad",
+  "Tanzania",
+  "Nigeria",
+  "Viet Nam",
+  "Ghana",
+  "India",
+  "Mali",
+  "Pakistan",
+  "Peru"
+)
+emerging_ct <- c("Brazil",
+                 "China",
+                 "Thailand",
+                 "Russian Federation",
+                 "India",
+                 "Kazakhstan",
+                 "Nigeria")
+highdeveloped_ct <- c(
+  "United States",
+  "United Kingdom",
+  "New Zealand",
+  "Finland",
+  "Czechia",
+  "Aruba",
+  "Qatar"
+)
+# extra column for categorization
+tobacco_df$dev_status <- ifelse(
+  tobacco_df$`Country Name` %in% development_ct,
+  "development countries",
+  ifelse(
+    tobacco_df$`Country Name` %in% emerging_ct,
+    "emerging countries",
+    ifelse(
+      tobacco_df$`Country Name` %in% highdeveloped_ct,
+      "highly developed countries",
+      "Unbekannt"
+    )
+  )
+)
+# Because the grouping by development status is of relative fresh data, we only
+# take a look at year 2020
+tobacco_dev_df <- tobacco_df %>%
+  filter(Year == 2020)
+# Dataframe of grouped countries and their value
+tobacco_dev_df <- tobacco_dev_df %>%
+  group_by(dev_status) %>%
+  summarise(
+    NY.GDP.PCAP.PP.KD = mean(NY.GDP.PCAP.PP.KD, na.rm = TRUE),
+    SH.PRV.SMOK = mean(SH.PRV.SMOK, na.rm = TRUE)
+  )
+
+#b)
+## 2b)
+continent_mapping <- data.frame(
+  Country = c(
+    'Afghanistan',
+    'Aruba',
+    'Bangladesh',
+    'Bolivia',
+    'Brazil',
+    'Cambodia',
+    'Chad',
+    'Russian Federation',
+    'Tanzania',
+    'Thailand',
+    'United States',
+    'United Kingdom',
+    'Nigeria',
+    'Viet Nam',
+    'China',
+    'Czechia',
+    'Finland',
+    'Ghana',
+    'India',
+    'Kazakhstan',
+    'Mali',
+    'New Zealand',
+    'Pakistan',
+    'Peru',
+    'Qatar'
+  ),
+  continent = c(
+    'Asien',
+    'Nordamerika',
+    'Asien',
+    'Südamerika',
+    'Südamerika',
+    'Asien',
+    'Afrika',
+    'Europa',
+    'Afrika',
+    'Asien',
+    'Nordamerika',
+    'Europa',
+    'Afrika',
+    'Asien',
+    'Asien',
+    'Europa',
+    'Europa',
+    'Afrika',
+    'Asien',
+    'Asien',
+    'Afrika',
+    'Ozeanien',
+    'Asien',
+    'Südamerika',
+    'Asien'
+  )
+)
+# dataframe with continent variable
+tobacco_df <- tobacco_df %>%
+  left_join(continent_mapping, by = c("Country Name" = "Country"))
+# grouped dataframe with values by mean over all years
+tobacco_continent_df <- tobacco_df %>%
+  group_by(continent) %>%
+  summarise(
+    NY.GDP.PCAP.PP.KD = mean(NY.GDP.PCAP.PP.KD, na.rm = TRUE),
+    SH.PRV.SMOK = mean(SH.PRV.SMOK, na.rm = TRUE)
+  )
+# only year 2020
+tobacco_continent_2020_df <- tobacco_df %>%
+  filter(Year == 2020) %>%
+  group_by(continent) %>%
+  summarise(
+    NY.GDP.PCAP.PP.KD = mean(NY.GDP.PCAP.PP.KD, na.rm = TRUE),
+    SH.PRV.SMOK = mean(SH.PRV.SMOK, na.rm = TRUE)
+  )
+
 
 ## Check how many NA's are in the data to be used
 # No NA`s in GDP per capita
@@ -42,20 +183,20 @@ sum(is.na(full_data$NY.GDP.PCAP.PP.KD))
 sum(is.na(full_data$SH.PRV.SMOK))
 
 # Check how many NA's in tabacco each Year
-years_data_tobacco <- tobacco_df %>% 
-  group_by(Year) %>% 
-  reframe(num_na = sum(is.na(SH.PRV.SMOK))) %>% 
+years_data_tobacco <- tobacco_df %>%
+  group_by(Year) %>%
+  reframe(num_na = sum(is.na(SH.PRV.SMOK))) %>%
   filter(num_na < 25)
-# Knowing there are only 25 countries to investigate, we only have data of the 
-# Years 2000, 2005, 2010, 1015, 1018, 2019 and 2020 on percentage of tabacco 
+# Knowing there are only 25 countries to investigate, we only have data of the
+# Years 2000, 2005, 2010, 1015, 1018, 2019 and 2020 on percentage of tabacco
 # usage in the adult population of all 24 countries (excluding Aruba).
 
 # (first have to solve grouping of countries generally before performing this task:)
 # Exclude Aruba from the tabacco_df
 
 ## data in year:
-tobacco_year_df <- function(year){
-  tobacco_df %>% 
+tobacco_year_df <- function(year) {
+  tobacco_df %>%
     filter(Year %in% year)
 }
 # Check number of NA's in tabacco usage variable of Year k
@@ -81,10 +222,12 @@ sum(is.na(means_per_country$mean_tobacco))
 ggplot(means_per_country,
        aes(x = mean_gdp, y = mean_tobacco, colour = `Country Name`)) +
   geom_point(size = 2) +
-  labs(title = "Correlation of GDP per Capita and tobacco prevalence",
-       x = "GDP per Capita in $",
-       y = "Prevalence of tobacco use in % of adults",
-       colour = "Country")
+  labs(
+    title = "Correlation of GDP per Capita and tobacco prevalence",
+    x = "GDP per Capita in $",
+    y = "Prevalence of tobacco use in % of adults",
+    colour = "Country"
+  )
 
 ## 5) Bar chart comparing GDP to tabacco usage
 # Get an overview of tobacco variable
@@ -93,54 +236,53 @@ means_per_country %>%
   ggplot(aes(x = `Country Name`, y = mean_tobacco)) +
   geom_bar(stat = "identity") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(title = "Prävalenz des Tabakkonsums nach Land", x = "Land", 
-       y = "Prävalenz des Tabakkonsums in % der Erwachsenen")
+  labs(title = "Prävalenz des Tabakkonsums nach Land", x = "Land", y = "Prävalenz des Tabakkonsums in % der Erwachsenen")
 # Code for plotting GDP per Capita grouped by country
 tobacco_df %>%
   group_by(`Country Name`) %>%
   summarise(mean_gdp = mean(NY.GDP.PCAP.PP.KD)) %>%
   ggplot(aes(x = `Country Name`, y = mean_gdp)) +
   geom_bar(stat = "identity") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 13)) +
-  labs(title = "BIP pro Kopf nach Land", x = "Land", 
-       y = "BIP pro Kopf")
+  theme(axis.text.x = element_text(
+    angle = 45,
+    hjust = 1,
+    size = 13
+  )) +
+  labs(title = "BIP pro Kopf nach Land", x = "Land", y = "BIP pro Kopf")
 # put last two together:
 ## genius plot try (Skizze am Ipad!) not working :(
 # Aktuellste Daten im Jahr 2020
-tobacco_2020_df <- tobacco_df %>% 
+tobacco_2020_df <- tobacco_df %>%
   filter(Year %in% 2020)
 # negative axis of tobacco = SH.PRV.SMOK.NEG
 tobacco_2020_df$SH.PRV.SMOK.NEG <- -tobacco_2020_df$SH.PRV.SMOK
 ggplot(data = tobacco_2020_df) +
   geom_bar(aes(x = `Country Name`, y = NY.GDP.PCAP.PP.KD), stat = "identity") +
   geom_bar(aes(x = `Country Name`, y = SH.PRV.SMOK.NEG), stat = "identity") +
-  labs(
-    title = "Tabakkonsum im Vergleich zu BIP",
-    x = "Land",
-    y = NULL
-  ) +
-  scale_y_continuous(
-    name = "BIP pro Kopf in $",
-    sec.axis = sec_axis(~ -., name = "Tabakkonsum in % der Erwachsenen")
-  )
+  labs(title = "Tabakkonsum im Vergleich zu BIP", x = "Land", y = NULL) +
+  scale_y_continuous(name = "BIP pro Kopf in $",
+                     sec.axis = sec_axis( ~ -., name = "Tabakkonsum in % der Erwachsenen"))
 
 # 3)
 # Scatterplot der Daten von 2020 (aktuellste) -> Querschnittsdaten
-ggplot(tobacco_2020_df, aes(x = NY.GDP.PCAP.PP.KD, y = SH.PRV.SMOK, color = `Country Name`)) +
+ggplot(tobacco_2020_df,
+       aes(x = NY.GDP.PCAP.PP.KD, y = SH.PRV.SMOK, color = `Country Name`)) +
   geom_point() +
-  geom_smooth(method = "lm", se = FALSE, color = "blue") +
-  labs(x = "BIP pro Kopf in $", y = "Tabakkonsum in % der Erwachsenen", 
-       title = "Korrelation zwischen BIP und Tabakkonsum") +
+  geom_smooth(method = "lm",
+              se = FALSE,
+              color = "blue") +
+  labs(x = "BIP pro Kopf in $", y = "Tabakkonsum in % der Erwachsenen", title = "Korrelation zwischen BIP und Tabakkonsum") +
   theme_minimal()
 
 # 7) Scatterplot facettiert, x = Jahr, Entwicklung des Tabakkonsums (macht nicht was ich will)
 tobacco_noaruba_df <- tobacco_df[tobacco_df$`Country Name` != "Aruba", ]
 ggplot(tobacco_df, aes(x = Year, y = SH.PRV.SMOK)) +
   geom_point() +
-  geom_smooth(method = "lm", se = FALSE, color = "blue") +
-  facet_wrap(~ `Country Name`) +
-  labs(x = "BIP pro Kopf in $", y = "Tabakkonsum in % der Erwachsenen", 
-       title = "Korrelation pro Land über die Zeit") +
+  geom_smooth(method = "lm",
+              se = FALSE,
+              color = "blue") +
+  facet_wrap( ~ `Country Name`) +
+  labs(x = "BIP pro Kopf in $", y = "Tabakkonsum in % der Erwachsenen", title = "Korrelation pro Land über die Zeit") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   theme_minimal()
 
@@ -152,10 +294,67 @@ heatmap_data <- data %>%
 
 ggplot(heatmap_data, aes(x = Year, y = Country, fill = correlation)) +
   geom_tile() +
-  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
-  labs(x = "Jahr", y = "Land", fill = "Korrelation", title = "Korrelation über Länder und Jahre") +
+  scale_fill_gradient2(
+    low = "blue",
+    mid = "white",
+    high = "red",
+    midpoint = 0
+  ) +
+  labs(x = "Jahr",
+       y = "Land",
+       fill = "Korrelation",
+       title = "Korrelation über Länder und Jahre") +
   theme_minimal()
 # Have to compromise countries! Make subgroups
 
+## 8) data grouped by development state
+ggplot(tobacco_dev_df,
+       aes(NY.GDP.PCAP.PP.KD, SH.PRV.SMOK, colour = dev_status)) +
+  geom_point() +
+  labs(
+    title = "Tabakkonsum vs BIP im Jahr 2020",
+    x = "BIP pro Kopf",
+    y = "Anteil tabakkonsumierender Gesellschaft",
+    colour = "Entwicklungsstand"
+  )
+# als Vergleich alle Länder mit Daten nur codiert über Entwicklungsstand (Varianz, ...)
+ggplot(tobacco_2020_df,
+       aes(NY.GDP.PCAP.PP.KD, SH.PRV.SMOK, colour = dev_status)) +
+  geom_point() +
+  labs(
+    title = "Tabakkonsum vs BIP im Jahr 2020",
+    x = "BIP pro Kopf",
+    y = "Anteil tabakkonsumierender Gesellschaft",
+    colour = "Entwicklungsstand"
+  )
 
-# Sprache in Graphen auf deutsch anpassen
+## 9) data grouped by continent
+ggplot(tobacco_continent_df,
+       aes(NY.GDP.PCAP.PP.KD, SH.PRV.SMOK, colour = continent)) +
+  geom_point() +
+  labs(
+    title = "Tabakkonsum vs BIP im Durchschnitt aller erhobenen Jahr",
+    x = "BIP pro Kopf",
+    y = "Anteil tabakkonsumierender Gesellschaft",
+    colour = "Kontinent"
+  )
+# only most recent data
+ggplot(tobacco_continent_2020_df,
+       aes(NY.GDP.PCAP.PP.KD, SH.PRV.SMOK, colour = continent)) +
+  geom_point() +
+  labs(
+    title = "Tabakkonsum vs BIP im Jahr 2020",
+    x = "BIP pro Kopf",
+    y = "Anteil tabakkonsumierender Gesellschaft",
+    colour = "Kontinent"
+  )
+# for comparison now all countries only coded over continent (variance, sample size, ...)
+ggplot(tobacco_2020_df,
+       aes(NY.GDP.PCAP.PP.KD, SH.PRV.SMOK, colour = continent)) +
+  geom_point(size = 4, alpha = 0.5) +
+  labs(
+    title = "Tabakkonsum vs BIP im Jahr 2020",
+    x = "BIP pro Kopf",
+    y = "Anteil tabakkonsumierender Gesellschaft",
+    colour = "Kontinent"
+  )
