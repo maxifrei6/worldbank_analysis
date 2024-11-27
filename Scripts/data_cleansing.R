306ae04069fad0d2eecc82ebb1db2af1ca0cb226
Robin Billinger <135109065+rb-bllngr@users.noreply.github.com>
2024-11-27 20:07:39 +0100

    optimize variable names and add RDS file for data_merged

--- Scripts/data_cleansing.R
+++ Scripts/data_cleansing.R
@@ -28,104 +28,105 @@ library(checkmate)
 source("Scripts/functions.R")
 
 # Read the csv files from Data/Raw folder
-geodata <- read.csv("Data/Raw/Worldbank2.csv", header = TRUE, sep = ",", na.strings = "")
-co2data <- read.csv("Data/Raw/Co2_emi_WB.csv", header = TRUE, sep = ";", na.strings = "")
+data_geo <- read.csv("Data/Raw/Worldbank2.csv", header = TRUE, sep = ",", na.strings = "")
+data_co2 <- read.csv("Data/Raw/Co2_emi_WB.csv", header = TRUE, sep = ";", na.strings = "")
 
 # Remove only the first instance of a dot if followed by another dot in the number (co2 emissions format x.xxx.xxx)
-co2data <- co2data %>%
+data_co2 <- data_co2 %>%
   mutate(across(everything(), ~ str_replace(., "(?<=\\d)\\.(?=\\d+\\.\\d*)", "")))
 
-str(geodata)
-str(co2data)
-head(co2data)
+str(data_geo)
+str(data_co2)
+head(data_co2)
 
 # Read the xlsx file from Data/Raw folder
-sociometrics <- read_excel("Data/Raw/Worldbank1.xlsx", sheet = 1, col_names = TRUE, na = "..")
+data_sociometrics <- read_excel("Data/Raw/Worldbank1.xlsx", sheet = 1, col_names = TRUE, na = "..")
 
 # TODO: create dictionary/list from sheet 2 to match abbreviated metrics with description
 
 # Delete columns or rows if they are all NAs.
 # Apply the remove_NAs function to all interested datasets
-geodata <- remove_NA(geodata)
-sociometrics <- remove_NA(sociometrics)
-co2data <- remove_NA(co2data)
+data_geo <- remove_NA(data_geo)
+data_sociometrics <- remove_NA(data_sociometrics)
+data_co2 <- remove_NA(data_co2)
 
 # Remove duplicate rows if any
-geodata <- distinct(geodata)
-sociometrics <- distinct(sociometrics)
-co2data <- distinct(co2data)
+data_geo <- distinct(data_geo)
+data_sociometrics <- distinct(data_sociometrics)
+data_co2 <- distinct(data_co2)
 
 # Get an overview of the datasets
-str(geodata)
-summary(geodata)
-str(sociometrics)
-summary(sociometrics)
-str(co2data)
-summary(co2data)
+str(data_geo)
+summary(data_geo)
+str(data_sociometrics)
+summary(data_sociometrics)
+str(data_co2)
+summary(data_co2)
 
 # Modification to datasets, simplifying their joining
 # Colnames for years aligning in both datasets
-colnames(geodata) <- gsub(pattern = "X([[:digit:]]{4})..YR[[:digit:]]{4}.",
-                          replacement = "\\1", colnames(geodata))
-colnames(co2data) <- gsub(pattern = "X([[:digit:]]{4})..YR[[:digit:]]{4}.",
-                          replacement = "\\1", colnames(co2data))
-colnames(sociometrics) <- gsub(pattern = "([[:digit:]]{4}) \\[YR\\1\\]",
-                               replacement = "\\1", colnames(sociometrics))
+colnames(data_geo) <- gsub(pattern = "X([[:digit:]]{4})..YR[[:digit:]]{4}.",
+                          replacement = "\\1", colnames(data_geo))
+colnames(data_co2) <- gsub(pattern = "X([[:digit:]]{4})..YR[[:digit:]]{4}.",
+                          replacement = "\\1", colnames(data_co2))
+colnames(data_sociometrics) <- gsub(pattern = "([[:digit:]]{4}) \\[YR\\1\\]",
+                               replacement = "\\1", colnames(data_sociometrics))
 
-# Remaining colnames in geodata and co2data aligning with sociometrics, i.e. replacing dot between words with space
-geodata <- geodata %>%
+# Remaining colnames in data_geo and data_co2 aligning with data_sociometrics,
+# i.e. replacing dot between words with space
+data_geo <- data_geo %>%
   mutate(across(where(is.character))) %>%
   rename_with(~ gsub(pattern = "\\.", replacement = " ", .))
-co2data <- co2data %>%
+data_co2 <- data_co2 %>%
   mutate(across(where(is.character))) %>%
   rename_with(~ gsub(pattern = "\\.", replacement = " ", .))
 
 # Removing metadata rows
 metadata_source <- "Data from database: World Development Indicators" 
 metadata_time <- "Last Updated: 10/24/2024"
-geodata <- geodata[geodata$`Country Name` != metadata_source &
-                     geodata$`Country Name` != metadata_time, ]
-co2data <- co2data[co2data$`Country Name` != metadata_source &
-                     co2data$`Country Name` != metadata_time, ]
-sociometrics <- sociometrics[sociometrics$`Country Name` != metadata_source &
-                               sociometrics$`Country Name` != metadata_time, ]
+data_geo <- data_geo[data_geo$`Country Name` != metadata_source &
+                     data_geo$`Country Name` != metadata_time, ]
+data_co2 <- data_co2[data_co2$`Country Name` != metadata_source &
+                     data_co2$`Country Name` != metadata_time, ]
+data_sociometrics <- data_sociometrics[data_sociometrics$`Country Name` != metadata_source &
+                               data_sociometrics$`Country Name` != metadata_time, ]
 
 
 
 # TODO (???)
-str(geodata)
-str(sociometrics)
-str(co2data)
+str(data_geo)
+str(data_sociometrics)
+str(data_co2)
 
 # Delete column "average" and "Series Name" to avoid data type issues
-geodata <- geodata %>%
+data_geo <- data_geo %>%
   select(c(-average, -`Series Name`))
-sociometrics <- sociometrics %>%
+data_sociometrics <- data_sociometrics %>%
   select(c(-average, -`Series Name`))
-co2data <- co2data %>%
+data_co2 <- data_co2 %>%
   select(c(-average, -`Series Name`))
 
 # Ensure all year columns  are numeric to avoid merge errors
-geodata <- geodata %>%
+data_geo <- data_geo %>%
   mutate(across(matches("^[0-9]{4}$"), as.numeric))
-sociometrics <- sociometrics %>%
+data_sociometrics <- data_sociometrics %>%
   mutate(across(matches("^[0-9]{4}$"), as.numeric))
-co2data <- co2data %>%
+data_co2 <- data_co2 %>%
   mutate(across(matches("^[0-9]{4}$"), as.numeric))
 
 # Merge both datasets into one full dataset containing all information
-full_data <- full_join(geodata, sociometrics, by = colnames(geodata)) %>%
-  full_join(co2data, by = colnames(geodata))
+data_merged <- full_join(data_geo, data_sociometrics, by = colnames(data_geo)) %>%
+  full_join(data_co2, by = colnames(data_geo))
 
 # Sort the joined dataset by Country Name
-full_data <- sort_by(full_data, full_data$`Country Name`)
+data_merged <- sort_by(data_merged, data_merged$`Country Name`)
 
 
 # TODO
 
 
 # Columns with Years in column "Year"
-full_data <- full_data %>%
+data_merged <- data_merged %>%
   pivot_longer(
     cols = starts_with("20"),
     names_to = "Year",
@@ -134,7 +135,7 @@ full_data <- full_data %>%
   )
 
 # Variables in "Series Code" as columns
-full_data <- full_data %>%
+data_merged <- data_merged %>%
   pivot_wider(
     id_cols = c(`Country Name`, `Country Code`, Year),
     names_from = `Series Code`,
@@ -142,4 +143,4 @@ full_data <- full_data %>%
   )
 
 # Save the merged dataset for future use in the processed data directory
-############ saveRDS(full_data, file = "Data/Processed/full_data.RDS")
+saveRDS(data_merged, file = "Data/Processed/data_merged.RDS")
