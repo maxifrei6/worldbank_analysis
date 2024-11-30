c9613e25d76748c3573174d3672e7758ff2d7435
Robin Billinger <135109065+rb-bllngr@users.noreply.github.com>
2024-11-30 12:43:26 +0100

    change variable names inside agriculture script

--- Scripts/agriculture.R
+++ Scripts/agriculture.R
@@ -1,9 +1,8 @@
 # Load libraries
 # TODO
 
-# Load the "_____" dataset
-# TODO
-data_full <- readRDS("Data/Processed/data_full.RDS")
+# Load the processed "data_merged" dataset
+data_agriculture <- readRDS("Data/Processed/data_merged.RDS")
 
 # Questions:
 # 1. Is there a relationship between the percentage of agricultural land and
@@ -33,18 +32,18 @@ data_full <- readRDS("Data/Processed/data_full.RDS")
 # 2.1) 
 # 2.2) 
 
-# Filter relevant columns Country Name, Country Code, Year, AG.LND.AGRI.ZS, AG.SRF.TOTL.K2 and EN.GHG.CO2.MT.CE.AR5	 
-agriculture <- full_data %>%
+# Filter relevant columns Country Name, Country Code, Year, AG.LND.AGRI.ZS, AG.SRF.TOTL.K2 and EN.GHG.CO2.MT.CE.AR5
+data_agriculture <- data_agriculture %>%
   select(`Country Name`, `Country Code`, Year, AG.LND.AGRI.ZS, AG.SRF.TOTL.K2, EN.GHG.CO2.MT.CE.AR5)
 
 # Check whether any missing values in interested data subset
-all(!is.na(agriculture))
+all(!is.na(data_agriculture))
 
 # Convert column `Year` to data type Integer
-agriculture$Year <- as.integer(agriculture$Year)
+data_agriculture$Year <- as.integer(data_agriculture$Year)
 
 # Normalize variables to same scale
-agriculture_normd <- agriculture %>%
+data_agriculture_normd <- data_agriculture %>%
   group_by(`Country Name`) %>%
   mutate(
     co2_min = min(EN.GHG.CO2.MT.CE.AR5),
@@ -53,15 +52,15 @@ agriculture_normd <- agriculture %>%
   ) %>%
   ungroup()
 
-# Pivot normalized agriculture dataset to longer format for better visualization of two variables (Idea 1.7)
-agriculture_normd_long <- agriculture_normd %>%
+# Pivot normalized data_agriculture dataset to longer format for better visualization of two variables (Idea 1.7)
+data_agriculture_normd_long <- data_agriculture_normd %>%
   pivot_longer(cols = c(co2_normd, AG.LND.AGRI.ZS), names_to = "variable", values_to = "value") %>%
   mutate(value = case_when(
     variable == "co2_normd" ~ value * 100,  # used for [%] comparison
     variable == "AG.LND.AGRI.ZS" ~ value))
 
 # Compute the average change in agricultural land [%] by country
-country_order_by_agriland <- agriculture %>%
+country_order_by_agriland <- data_agriculture %>%
   group_by(`Country Name`) %>%
   summarise(AG.LND.AGRI.ZS_avg = mean(AG.LND.AGRI.ZS)) %>%
   arrange(AG.LND.AGRI.ZS_avg) %>%
@@ -70,7 +69,7 @@ country_order_by_agriland <- agriculture %>%
 
 
 ### Idea 1.1: Scatter plot with agricultural land [%] as color
-ggplot(agriculture, aes(colour = AG.LND.AGRI.ZS)) +
+ggplot(data_agriculture, aes(colour = AG.LND.AGRI.ZS)) +
   geom_point(aes(x = Year, y = EN.GHG.CO2.MT.CE.AR5,)) +
   labs(title = "Relationship between agricultural land and CO2 emissions per capita",
        x = "Year", y = "y") +
@@ -82,7 +81,7 @@ ggplot(agriculture, aes(colour = AG.LND.AGRI.ZS)) +
 
 
 ### Idea 1.2: Scatter Plot with Country as Color
-ggplot(agriculture, aes(x = AG.LND.AGRI.ZS, y = log10(EN.GHG.CO2.MT.CE.AR5 +1))) +
+ggplot(data_agriculture, aes(x = AG.LND.AGRI.ZS, y = log10(EN.GHG.CO2.MT.CE.AR5 +1))) +
   geom_point(aes(colour = `Country Name`), alpha = .5) +
   labs(title = "Relationship between agricultural land and CO2 emissions per capita",
        x = "Agricultural land [%]", fill = "Country") +
@@ -101,7 +100,7 @@ ggplot(agriculture, aes(x = AG.LND.AGRI.ZS, y = log10(EN.GHG.CO2.MT.CE.AR5 +1)))
 
 
 ### Idea 3: Heatmap (only CO2 emissions though) in table design
-ggplot(agriculture, aes(x = Year, y = `Country Name`, fill = log10(EN.GHG.CO2.MT.CE.AR5 + 1))) +
+ggplot(data_agriculture, aes(x = Year, y = `Country Name`, fill = log10(EN.GHG.CO2.MT.CE.AR5 + 1))) +
   geom_tile(col = "white") +  # Create heatmap tiles
   scale_fill_gradient(low = "white", high = "black", name = "Logarithmic CO2 Emissions\nper capita", 
                       breaks = log10(c(0, 100, 1000, 10000, 100000) + 1),
@@ -119,12 +118,12 @@ ggplot(agriculture, aes(x = Year, y = `Country Name`, fill = log10(EN.GHG.CO2.MT
 library(rnaturalearth)
 library(sf)
 worldmap <- ne_countries(scale = "medium", returnclass = "sf")
-agriculture_worldmap <- worldmap %>%
-  left_join(agriculture, by = c("iso_a3" = "Country Code"))
+data_agriculture_worldmap <- worldmap %>%
+  left_join(data_agriculture, by = c("iso_a3" = "Country Code"))
 # Filter out the NA countries to only show the countries of the interested dataset
-agriculture_worldmap <- agriculture_worldmap %>% filter(!is.na(Year))
+data_agriculture_worldmap <- data_agriculture_worldmap %>% filter(!is.na(Year))
 # Build five/six year timespans for each country
-agriculture_worldmap <- agriculture_worldmap %>%
+data_agriculture_worldmap <- data_agriculture_worldmap %>%
   mutate(timespan = case_when(
     Year >= 2000 & Year <= 2005 ~ "2000-2005",
     Year >= 2006 & Year <= 2010 ~ "2006-2010",
@@ -132,7 +131,7 @@ agriculture_worldmap <- agriculture_worldmap %>%
     Year >= 2016 & Year <= 2021 ~ "2016-2021"
   ))
 # Plot the worldmap 
-ggplot(agriculture_worldmap) +
+ggplot(data_agriculture_worldmap) +
   geom_sf(aes(fill = log10(EN.GHG.CO2.MT.CE.AR5 + 1))) +
   scale_fill_gradient(low = "white", high = "blue", name = "Logarithmierte CO2 Emissionen pro Kopf", 
                       breaks = log10(c(0, 100, 1000, 10000, 100000) + 1),
@@ -150,7 +149,7 @@ ggplot(agriculture_worldmap) +
 
 
 ### Idea 1.5: Logarithmic CO2 emissions by country, average agricultural land [%] for color and facet order
-ggplot(agriculture, aes(colour = AG.LND.AGRI.ZS)) +
+ggplot(data_agriculture, aes(colour = AG.LND.AGRI.ZS)) +
   geom_point(aes(x = Year, y = log10(EN.GHG.CO2.MT.CE.AR5 + 1))) +
   scale_color_continuous(name = "Landwirtschaftliche Nutzfläche [%]",
                          type = "viridis",
@@ -162,12 +161,12 @@ ggplot(agriculture, aes(colour = AG.LND.AGRI.ZS)) +
   theme(legend.position = "bottom",
         plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(angle = 45, hjust = 1)) +
-  facet_wrap(~ factor(agriculture$`Country Name`, levels = country_order_by_agriland), ncol = 5)
+  facet_wrap(~ factor(data_agriculture$`Country Name`, levels = country_order_by_agriland), ncol = 5)
 
 
 
 ### Idea 1.6: Normalized and logarithmic CO2 emissions by country, avg. agricultural land [%] for color and facet order
-ggplot(agriculture_normd, aes(colour = AG.LND.AGRI.ZS)) +
+ggplot(data_agriculture_normd, aes(colour = AG.LND.AGRI.ZS)) +
   geom_point(aes(x = Year, y = co2_normd)) +  #
   scale_color_continuous(name = "Landwirtschaftliche Nutzfläche [%]",
                          type = "viridis",
@@ -179,12 +178,12 @@ ggplot(agriculture_normd, aes(colour = AG.LND.AGRI.ZS)) +
   theme(legend.position = "bottom",
         plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(angle = 45, hjust = 1)) +
-  facet_wrap(~ factor(agriculture_normd$`Country Name`, levels = country_order_by_agriland), ncol = 5)
+  facet_wrap(~ factor(data_agriculture_normd$`Country Name`, levels = country_order_by_agriland), ncol = 5)
 
 
 
 ### Idea 1.7: Line plots with normalized data over time for each country
-ggplot(agriculture_normd_long,
+ggplot(data_agriculture_normd_long,
        aes(x = Year, y = value, color = variable, group = interaction(`Country Name`, variable))) +
   geom_point() +  # or geom_line? (And is geom_line overdramatizing the (wrong?) outliers in CO2 emissions?)
   labs(title = "Zusammenhang zwischen CO2 Emissionen pro Kopf und landwirtschaftlicher Nutzfläche",
@@ -205,4 +204,11 @@ ggplot(agriculture_normd_long,
 
 
 
-### Idea 1.8: Boxplots 
+### Idea 1.8: Boxplots
+
+
+
+##############################################################################################################
+# Grouping by surface area:
+# TODO: CHANGE NAME OF 
+dummy <- readRDS("Data/Processed/data_merged.RDS")
