08622c24994a4b94952cfc8c22ebae6097863b8e
Robin Billinger <135109065+rb-bllngr@users.noreply.github.com>
2024-12-08 22:35:05 +0100

    readjust CO2 data import (1/x)

--- Scripts/data_cleansing.R
+++ Scripts/data_cleansing.R
@@ -3,19 +3,14 @@
 ## OUTLINE
 # Data preparation
   # Load data
+  # Adjustments to correct data inconsistencies for CO2-dataset
   # Initial NA removal
   # Data overview
 
-# Data modification
+# Data modification and merging
   # Renaming and selecting columns
   # Removing metadata rows at end of dataset
-
-# TODO (???)
-
-# Data Merging and Cleaning
   # Merging datasets
-
-# TODO
 
 # Final steps 
   # Save cleaned datasets for future use
@@ -31,9 +26,40 @@ source("Scripts/functions.R")
 data_geo <- read.csv("Data/Raw/Worldbank2.csv", header = TRUE, sep = ",", na.strings = "")
 data_co2 <- read.csv("Data/Raw/Co2_emi_WB.csv", header = TRUE, sep = ";", na.strings = "")
 
-# Remove only the first instance of a dot if followed by another dot in the number (co2 emissions format x.xxx.xxx)
+###################################################################################################################
+#################### NEW WAY TO READ IN AND FORMAT THE CO2 EMISSIONS FROM 2024-12-09 ONWARDS: #####################
+# Remove all dots in the CO2 data entries
 data_co2 <- data_co2 %>%
-  mutate(across(everything(), ~ str_replace(., "(?<=\\d)\\.(?=\\d+\\.\\d*)", "")))
+  mutate(across(.cols = matches("^X(2000|20[0-1][0-9]|2020|2021)..YR(2000|20[0-1][0-9]|2020|2021)\\.$"),
+                .fns = ~ gsub(pattern = "\\.", replacement = "", .)))
+
+# If the data entries start with zero, the remaining digits behind the zero are decimal places. Therefore, put a
+# dot behind the zero if it is a leading zero.
+data_co2 <- data_co2 %>%
+  mutate(across(.cols = matches("^X(2000|20[0-1][0-9]|2020|2021)..YR(2000|20[0-1][0-9]|2020|2021)\\.$"),
+                .fns = ~ gsub(pattern = "^0", replacement = "0.", .)))
+
+# For all other data entries, insert a dot after fourth digit from the right if number has more than four digits
+# or after third digit from the right if number has at most four digits, thereby converting it to decimal number.
+data_co2 <- data_co2 %>%
+  mutate(across(.cols = matches("^X(2000|20[0-1][0-9]|2020|2021)..YR(2000|20[0-1][0-9]|2020|2021)\\.$"),
+                .fns = ~ {
+                  # Apply transformation only to entries that do not start with zero
+                  ifelse(substr(., 1, 1) != "0",
+                    ifelse(nchar(.) > 4,
+                      # Insert the dot after the 4th digit and convert to numeric
+                      as.numeric(substr(., 1, nchar(.) - 4)) + as.numeric(substr(., nchar(.) - 3, nchar(.))) / 10000,
+                      # Insert the dot after the 3rd digit and convert to numeric
+                      as.numeric(substr(., 1, nchar(.) - 3)) + as.numeric(substr(., nchar(.) - 2, nchar(.))) / 1000),
+                    # Keep entries starting with zero unchanged
+                    as.numeric(.))
+                }))
+
+
+# TODO - IDEA: GO THROUGH EACH ROW (=EACH COUNTRY) AND CHECK THE RELATIVE CHANGE FROM YEAR TO YEAR. 
+# IF THE CHANGE IS LARGER THAN (E.G.) -50%, THEN YOU ADD A TRAILING ZERO TO THE AS.CHARACTER()-ENTRY.
+# AFTERWARDS YOU REMOVE THE CURRENT DOT AND SET A NEW DOT AT THE CORRECTED SPOT FOR 4 DECIMAL PLACES.
+###################################################################################################################
 
 str(data_geo)
 str(data_co2)
@@ -86,14 +112,12 @@ data_co2 <- data_co2[data_co2$`Country Name` != metadata_source &
 data_sociometrics <- data_sociometrics[data_sociometrics$`Country Name` != metadata_source &
                                data_sociometrics$`Country Name` != metadata_time, ]
 
-
-
-# TODO (???)
+# Get an overview of the adjusted datasets
 str(data_geo)
 str(data_sociometrics)
 str(data_co2)
 
-# create and save RDS before deleting averages
+# Create and save RDS before deleting averages
 data_pregrouped <- full_join(data_geo, data_sociometrics, by = colnames(data_geo))
 saveRDS(data_pregrouped, file = "Data/Processed/data_pregrouped.RDS")
 
@@ -119,10 +143,6 @@ data_merged <- full_join(data_geo, data_sociometrics, by = colnames(data_geo)) %
 
 # Sort the joined dataset by Country Name
 data_merged <- sort_by(data_merged, data_merged$`Country Name`)
-
-
-# TODO
-
 
 # Columns with Years in column "Year"
 data_merged <- data_merged %>%
