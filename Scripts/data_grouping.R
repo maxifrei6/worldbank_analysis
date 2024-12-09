6ca30d4cd7817b3bb8d2aea19fa3748883a34909
maxifrei6 <maximilian.frei@campus.lmu.de>
2024-12-10 00:44:47 +0100

    used function for country grouping

--- Scripts/data_grouping.R
+++ Scripts/data_grouping.R
@@ -6,113 +6,6 @@ library(checkmate)
 source("Scripts/functions.R")
 
 
-## 1. Grouping by population criteria
-# Load and filter the data
-data_grouped <- readRDS("Data/Processed/data_pregrouped.RDS") %>%
-  select(`Country Name`,
-         `Country Code`,
-         `Series Name`,
-         `Series Code`,
-         `average`)
-
-data_grouped_pop <- data_grouped %>%
-  filter(`Series Code` == "SP.POP.TOTL")
-
-# Calculate the 1/3 and 2/3 quantiles of the 'average' column
-quantiles <- quantile(data_grouped_pop$average,
-                      probs = c(1 / 3, 2 / 3),
-                      na.rm = TRUE)
-
-# Add new column 'population_size' as an ordered factor
-data_grouped_pop <- data_grouped_pop %>%
-  mutate(
-    population_size = case_when(
-      average <= quantiles[1] ~ "Low",
-      average <= quantiles[2] ~ "Medium",
-      TRUE ~ "High"
-    ),
-    # Convert to ordered factor with levels High > Medium > Low
-    population_size = factor(
-      population_size,
-      levels = c("High", "Medium", "Low"),
-      ordered = TRUE
-    )
-  )
-
-# same for pop_density
-data_grouped_pop_density <- data_grouped %>%
-  filter(`Series Code` == "EN.POP.DNST")
-
-# Calculate the 1/3 and 2/3 quantiles of the 'average' column
-quantiles <- quantile(data_grouped_pop_density$average,
-                      probs = c(1 / 3, 2 / 3),
-                      na.rm = TRUE)
-
-print(quantiles)
-
-# Add new column 'population_density' as an ordered factor
-data_grouped_pop_density <- data_grouped_pop_density %>%
-  mutate(
-    population_density = case_when(
-      average <= quantiles[1] ~ "Low",
-      average <= quantiles[2] ~ "Medium",
-      TRUE ~ "High"
-    ),
-    # Convert to ordered factor with levels High > Medium > Low
-    population_density = factor(
-      population_density,
-      levels = c("High", "Medium", "Low"),
-      ordered = TRUE
-    )
-  )
-
-# same for income
-data_grouped_income_levels <- data_grouped %>%
-  filter(`Series Code` == "NY.ADJ.NNTY.PC.CD")
-
-# Calculate the 1/3 and 2/3 quantiles of the 'average' column
-quantiles <- quantile(data_grouped_income_levels$average,
-                      probs = c(1 / 3, 2 / 3),
-                      na.rm = TRUE)
-
-print(quantiles)
-
-# Add new column 'income_level' as an ordered factor
-data_grouped_income_levels <- data_grouped_income_levels %>%
-  mutate(
-    income_level = case_when(
-      average <= quantiles[1] ~ "Low",
-      average <= quantiles[2] ~ "Medium",
-      TRUE ~ "High"
-    ),
-    # Convert to ordered factor with levels High > Medium > Low
-    income_level = factor(
-      income_level,
-      levels = c("High", "Medium", "Low"),
-      ordered = TRUE
-    )
-  )
-
-# select relevant columns
-data_grouped_pop <- data_grouped_pop %>%
-  select(`Country Name`, `Country Code`, `population_size`)
-data_grouped_pop_density <- data_grouped_pop_density %>%
-  select(`Country Name`, `Country Code`, `population_density`)
-data_grouped_income_levels <- data_grouped_income_levels %>%
-  select(`Country Name`, `Country Code`, `income_level`)
-
-# merge all groups
-data_grouped <- left_join(data_grouped_pop,
-                          data_grouped_pop_density,
-                          by = c("Country Name", "Country Code")) %>%
-  left_join(data_grouped_income_levels,
-            by = c("Country Name", "Country Code"))
-
-
-
-
-
-
 ##############################################################################################################
 ## processed data frame for everyone to work with 
 data_processed <- readRDS(
@@ -141,8 +34,20 @@ data_grouped_tob <- group_data(df = data_processed, grouping_by = "SH.PRV.SMOK",
                                column_new = "tob_usage", quantile_labels = 
                                  c("Very Low", "Low", "Medium", "High", "Very High"))
 
-# 4. Grouping by population criteria (Maxi)
+# 4. Grouping by population size
+data_grouped_pop <- group_data(df = data_processed, grouping_by = "SP.POP.TOTL",
+                               column_new = "population_size", quantile_labels = 
+                                 c("Low", "Medium", "High"))
 
+# 5. Grouping by population density
+data_grouped_pop_density <- group_data(df = data_processed, grouping_by = "EN.POP.DNST",
+                               column_new = "population_density", quantile_labels = 
+                                 c("Low", "Medium", "High"))
+
+# 5. Grouping by income levels
+data_grouped_income_levels <- group_data(df = data_processed, grouping_by = "NY.ADJ.NNTY.PC.CD",
+                               column_new = "income_level", quantile_labels = 
+                                 c("Low", "Medium", "High"))
 
 # Merge all groups
 # TODO: all data groupings merge in one session with electricity, education, hiv, tobacco and agriculture. 
