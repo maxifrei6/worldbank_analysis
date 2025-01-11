2794820949d6b57f81571c814a9950beb909c273
Robin Billinger <135109065+rb-bllngr@users.noreply.github.com>
2025-01-11 14:53:58 +0100

    add type conversion for year column to cleansing and override .RDS file

--- Scripts/data_cleansing.R
+++ Scripts/data_cleansing.R
@@ -33,22 +33,25 @@ data_co2 <- read.csv(here("Data", "Raw", "Co2_emi_WB.csv"),
 # Remove all dots in the CO2 data entries.
 data_co2 <- data_co2 %>%
   mutate(across(
-    .cols = matches("^X(2000|20[0-1][0-9]|2020|2021)..YR(2000|20[0-1][0-9]|2020|2021)\\.$"),
+    .cols =
+      matches("^X(2000|20[0-1][0-9]|2020|2021)..YR(2000|20[0-1][0-9]|2020|2021)\\.$"),
     .fns = ~ gsub(pattern = "\\.", replacement = "", .)))
 
-# If the data entries start with zero, the remaining digits behind the zero are decimal places.
-# Therefore, put a dot behind the zero if it is a leading zero.
+# If the data entries start with zero, the remaining digits behind the zero are decimal
+# places. Therefore, put a dot behind the zero if it is a leading zero.
 data_co2 <- data_co2 %>%
   mutate(across(
-    .cols = matches("^X(2000|20[0-1][0-9]|2020|2021)..YR(2000|20[0-1][0-9]|2020|2021)\\.$"),
+    .cols =
+      matches("^X(2000|20[0-1][0-9]|2020|2021)..YR(2000|20[0-1][0-9]|2020|2021)\\.$"),
     .fns = ~ gsub(pattern = "^0", replacement = "0.", .)))
 
-# For all other data entries, insert a dot after fourth digit from the right if number has more
-# than four digits or after third digit from the right if number has at most four digits, thereby
-# converting it to decimal number.
+# For all other data entries, insert a dot after fourth digit from the right if number has
+# more than four digits or after third digit from the right if number has at most four
+# digits, thereby converting it to decimal number.
 data_co2 <- data_co2 %>%
   mutate(across(
-    .cols = matches("^X(2000|20[0-1][0-9]|2020|2021)..YR(2000|20[0-1][0-9]|2020|2021)\\.$"),
+    .cols =
+      matches("^X(2000|20[0-1][0-9]|2020|2021)..YR(2000|20[0-1][0-9]|2020|2021)\\.$"),
     .fns = ~ {
       # Apply transformation only to entries that do not start with zero.
       ifelse(substr(., 1, 1) != "0",
@@ -69,12 +72,14 @@ head(data_co2)
 
 # Read the .xlsx file using 'here'.
 data_sociometrics <-
-  read_excel(here("Data", "Raw", "Worldbank1.xlsx"), sheet = 1, col_names = TRUE, na = "..")
+  read_excel(here("Data", "Raw", "Worldbank1.xlsx"),
+             sheet = 1, col_names = TRUE, na = "..")
 
 # TODO: CREATE DICTIONARY/LIST FROM SHEET 2 TO MATCH ABBREVIATED METRICS WITH DESCRIPTION
 
-# Delete columns or rows if they consist of only NAs. Apply 'remove_NA' to all interested data sets
-# using 'list2env' to automatically update the inputted variables without manual assignment.
+# Delete columns or rows if they consist of only NAs. Apply 'remove_NA' to all interested
+# data sets using 'list2env' to automatically update the inputted variables without manual
+# assignment.
 list2env(remove_NA(data_geo, data_co2, data_sociometrics), envir = .GlobalEnv)
 
 # Get an overview of the data sets.
@@ -94,24 +99,27 @@ colnames(data_co2) <- gsub(pattern = "X([[:digit:]]{4})..YR[[:digit:]]{4}.",
 colnames(data_sociometrics) <- gsub(pattern = "([[:digit:]]{4}) \\[YR\\1\\]",
                                replacement = "\\1", colnames(data_sociometrics))
 
-# Remaining column names in data_geo and data_co2 aligning with data_sociometrics, i.e. replacing
-# dot between words with space. Apply 'align_colnames' to all interested data sets using 'list2env'
-# to automatically update the inputted variables without manual assignment.
+# Remaining column names in data_geo and data_co2 aligning with data_sociometrics, i.e.
+# replacing dot between words with space. Apply 'align_colnames' to all interested data
+# sets using 'list2env' to automatically update the inputted variables without manual
+# assignment.
 list2env(align_colnames(data_geo, data_co2, data_sociometrics), envir = .GlobalEnv)
 
-# Removing metadata rows by applying 'remove_metadata' to all interested data sets using 'list2env'
-# to automatically update the inputted variables without manual assignment.
-list2env(remove_metadata(metadata_source = "Data from database: World Development Indicators",
-                         metadata_time = "Last Updated: 10/24/2024",
-                         data_geo, data_co2, data_sociometrics), envir = .GlobalEnv)
+# Removing metadata rows by applying 'remove_metadata' to all interested data sets using
+# 'list2env' to automatically update the inputted variables without manual assignment.
+list2env(
+  remove_metadata(metadata_source = "Data from database: World Development Indicators",
+                  metadata_time = "Last Updated: 10/24/2024",
+                  data_geo, data_co2, data_sociometrics),
+  envir = .GlobalEnv)
 
 # Get an overview of the adjusted data sets.
 str(data_geo)
 str(data_sociometrics)
 str(data_co2)
 
-# Define the translation mapping for the 'Country Name' column to German for plot visualization
-# purposes at later point.
+# Define the translation mapping for the 'Country Name' column to German for plot
+# visualization purposes at later point.
 translation_mapping <- c("Bangladesh" = "Bangladesch",
                          "Bolivia" = "Bolivien",
                          "Brazil" = "Brasilien",
@@ -129,10 +137,12 @@ translation_mapping <- c("Bangladesh" = "Bangladesch",
                          "United States" = "USA",
                          "Viet Nam" = "Vietnam")
 
-# Change the 'Country Name' column by applying 'translate' to all interested data sets using
-# 'list2env' to automatically update the inputted variables without manual assignment.
-list2env(translate(translation_mapping, "Country Name", data_geo, data_co2, data_sociometrics),
-         envir = .GlobalEnv)
+# Change the 'Country Name' column by applying 'translate' to all interested data sets
+# using 'list2env' to automatically update the inputted variables without manual
+# assignment.
+list2env(translate(
+  translation_mapping, "Country Name", data_geo, data_co2, data_sociometrics),
+  envir = .GlobalEnv)
 
 # Join the data sets together.
 data_pregrouped <- full_join(data_geo, data_sociometrics, by = colnames(data_geo))
@@ -143,9 +153,10 @@ data_pregrouped <- data_pregrouped %>% arrange(`Country Name`)
 # Create the RDS before deleting averages and save the data set using 'here'.
 saveRDS(data_pregrouped, file = here("Data", "Processed", "data_pregrouped.RDS"))
 
-# Delete column 'average' and 'Series Name' to avoid data type issues and ensure all year columns
-# are numeric to avoid merge errors by applying 'prepare_join' to all interested data sets using
-# 'list2env' to automatically update the inputted variables without manual assignment.
+# Delete column 'average' and 'Series Name' to avoid data type issues and ensure all year
+# columns are numeric to avoid merge errors by applying 'prepare_join' to all interested
+# data sets using 'list2env' to automatically update the inputted variables without manual
+# assignment.
 list2env(prepare_join(data_geo, data_co2, data_sociometrics), envir = .GlobalEnv)
 
 # Merge both data sets into one full data set containing all information.
@@ -171,6 +182,9 @@ data_merged <- data_merged %>%
     names_from = `Series Code`,
     values_from = Value
   )
+
+# Convert the column 'Year' to data type integer.
+data_merged$Year <- as.integer(data_merged$Year)
 
 # Save the merged data set using 'here' for the file path.
 saveRDS(data_merged, file = here("Data", "Processed", "data_merged.RDS"))
